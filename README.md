1965623e1bf503fb8f7418486c1e3e242d4965ca
Raphaela Rudnik <Rapharudnik@gmail.com>
2025-11-02 21:54:42 +0000

    terceira atualização

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Tripleten web_project_around
+
+o projeto é uma pagina para representar uma rede social, foram implementadas funções como formulário, programação com java script (abre formulário quando clicamos no botão editar, preenche campos de nome e função, imprime na página essas novas informações quando clicamos em salvar).
+
+Manipulação do DOM com querySelector() e addEventListener()
+Eventos de formulário (submit, click)
+Modais/Pop-ups com abertura e fechamento dinâmicos
+Atualização de conteúdo em tempo real
+
+https://github.com/rapharudnik/web_project_around.git
---
+++ script.js
@@ -0,0 +1,52 @@
+// Selecionar os elementos principais
+const editButton = document.querySelector(".profile__info-edit-button");
+const popup = document.querySelector(".popup");
+const closeButton = document.querySelector(".popup__close-icon");
+
+// Função para abrir o popup
+function openPopup() {
+  popup.classList.add("popup_opened");
+}
+
+// Função para fechar o popup
+function closePopup() {
+  popup.classList.remove("popup_opened");
+}
+
+// Quando clicar no botão editar, abre o popup
+editButton.addEventListener("click", openPopup);
+
+// Quando clicar no botão fechar, fecha o popup
+closeButton.addEventListener("click", closePopup);
+
+const profilename = document.querySelector(".profile__name");
+const profilefunction = document.querySelector(".profile__info-function");
+
+const currentname = profilename.textContent;
+const currentfunction = profilefunction.textContent;
+
+const nameInput = document.querySelector("#form__input-name");
+const functionInput = document.querySelector("#form__input-function");
+
+nameInput.value = currentname;
+functionInput.value = currentfunction;
+
+let formElement = document.querySelector(".popup");
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  let nameInput = document.querySelector("#form__input-name");
+  let functionInput = document.querySelector("#form__input-function");
+
+  let newName = nameInput.value;
+  let newFunction = functionInput.value;
+
+  let profileName = document.querySelector(".profile__name");
+  let profileFunction = document.querySelector(".profile__info-function");
+
+  profileName.textContent = newName;
+  profileFunction.textContent = newFunction;
+}
+
+formElement.addEventListener("submit", handleProfileFormSubmit);
